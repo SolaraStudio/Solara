fc66bd6f0d800a20db3bcd2f33e4249365bc1b6d
Frostre_ <n9043395@gmail.com>
2026-08-28 22:35:13 +0200

    Update settings.gradle.kts for dependency management

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -1,15 +1,29 @@
-pluginManagement {
-    repositories {
-        google()
-        mavenCentral()
-        gradlePluginPortal()
-    }
-}
-dependencyResolutionManagement {
+buildscript {
     repositories {
         google()
         mavenCentral()
     }
+    dependencies {
+        classpath("com.android.tools.build:gradle:8.5.0")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.25")
+    }
 }
-rootProject.name = "Solara"
-include(":app")
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/SolaraStudio/Optima")
+            credentials {
+                username = project.findProperty("gpr.user") as? String ?: System.getenv("GITHUB_ACTOR")
+                password = project.findProperty("gpr.key") as? String ?: System.getenv("GITHUB_TOKEN")
+            }
+        }
+    }
+}
+
+tasks.register("clean", Delete::class) {
+    delete(rootProject.layout.buildDirectory)
+}
