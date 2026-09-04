92ed2ccfe3eed056841c4c6bc6600311f2fedcf1
Frostre_ <n9043395@gmail.com>
2026-09-04 11:40:29 +0200

    Refactor SolaraActivity to use OptimaEngine

--- android/app/src/main/java/com/solara/browser/SolaraActivity.kt
+++ android/app/src/main/java/com/solara/browser/SolaraActivity.kt
@@ -1,57 +1,55 @@
 package com.solara.browser
 
-import android.os.Build
 import android.os.Bundle
-import android.view.View
-import android.view.WindowInsets
-import android.view.WindowInsetsController
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import com.solara.browser.ui.layouts.BrowserScreen
+import com.solara.browser.ui.BrowserScreen
 import com.solara.browser.ui.theme.SolaraTheme
+import org.optima.OptimaEngine
 
 class SolaraActivity : ComponentActivity() {
+    private var engine: OptimaEngine? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        enableEdgeToEdge()
-        hideSystemBars()
-
         setContent {
             SolaraTheme {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    BrowserScreen()
+                    BrowserScreen(
+                        initialUrl = "https://example.com",
+                        onEngineReady = { loadedEngine ->
+                            engine = loadedEngine
+                        }
+                    )
                 }
             }
         }
     }
 
-    @Suppress("DEPRECATION")
-    private fun hideSystemBars() {
-        val decorView = window.decorView
+    override fun onPause() {
+        super.onPause()
+        engine?.pause()
+    }
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-            decorView.windowInsetsController?.apply {
-                hide(WindowInsets.Type.statusBars() or WindowInsets.Type.navigationBars())
-                systemBarsBehavior = WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
-            }
-        } else {
-            decorView.systemUiVisibility = (
-                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                    or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                    or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                    or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                    or View.SYSTEM_UI_FLAG_FULLSCREEN
-                    or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-                )
-        }
+    override fun onResume() {
+        super.onResume()
+        engine?.resume()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        engine?.destroy()
     }
 }
